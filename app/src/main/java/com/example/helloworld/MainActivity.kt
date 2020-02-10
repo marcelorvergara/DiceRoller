65892adc5953fcb000b1f6109e7951a745605f79
Marcelo <marcelorv@gmail.com>
2020-02-10 16:33:10 -0300

    first

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+        val clearButton: Button = findViewById(R.id.clear)
+        clearButton.setOnClickListener{ clear_all(diceImage,diceImage2) }
+    }
+
+    private fun clear_all(diceImage: ImageView,diceImage2: ImageView){
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
+
+    private fun rollDice() {
+
+        diceImage.setImageResource(random())
+        diceImage2.setImageResource(random())
+    }
+    private fun random():Int{
+        val randomInt = (1..6).random()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
+    }
+}
